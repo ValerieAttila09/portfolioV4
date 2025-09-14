71bb27d21db7fc4ae9d7275162432eb3a983913f
Valerie Attila Al-fath <akunvalerie76@gmail.com>
2025-09-14 15:00:23 +0000

    3D-Room-Improvization

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,6 +1,8 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useMediaQuery } from "react-responsive";
+import { Room } from "./Room";
+import HeroLights from "./HeroLights/HeroLights";
 
 export default function HeroExperience() {
 
@@ -9,8 +11,6 @@ export default function HeroExperience() {
 
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-      <ambientLight intensity={0.2} color={"#1a1a40"} />
-      <directionalLight position={[5, 5, 5]} intensity={5} />
 
       <OrbitControls
         enablePan={false}
@@ -21,10 +21,11 @@ export default function HeroExperience() {
         maxPolarAngle={Math.PI / 2}
       />
 
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={"teal"} />
-      </mesh>
+      <HeroLights />
+
+      <group scale={isMobile ? 0.7 : 1} position={[0, -3.5, 0]} rotation={[0, -Math.PI / 4, 0]}>
+        <Room />
+      </group>
     </Canvas>
   )
 }
